4f394bd1fa4d1167c0d8f9f14623b57c2b7d692c
Chioma Kamalu <kamaluchioma062@gmail.com>
2023-09-29 10:24:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Created on Wed Sep 27 06:14:07 2023
 
 import pickle
 from flask import Flask, render_template, request, escape
+from jinja2 import escape
 
 app = Flask(__name__, template_folder='templates')
 
